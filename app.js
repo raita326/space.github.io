014582f077afb3c7d6afa9106a3210288d567011
Rai <68686816+takadarai@users.noreply.github.com>
2020-09-23 20:09:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(window).bind("scroll", function() {
 	scrollHeight = $(document).height();
 	scrollPosition = $(window).height() + $(window).scrollTop();
-	if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.01) {
+	if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.005) {
         //スクロールの位置が下部5%の範囲に来た場合
         window.location.href = 'happening.html';
 	}
